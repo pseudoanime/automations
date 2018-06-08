4acf8c0c5931dd55b4a19ea57a36f034a7d12653
Lakshmi Balakrishnan <lbalakrishnan@brightsource.co.uk>
2018-06-08 11:41:30 +0000

    dorset

--- tests/Browser/dorsetCrealsTokensTest.php
+++ tests/Browser/dorsetCrealsTokensTest.php
@@ -13,15 +13,23 @@ class dorsetCrealsTokensTest extends DuskTestCase
      *
      * @return void
      */
-    public function testExample()
+    public function test_play_token()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('https://www.dorsetcereals.co.uk/collect-and-win/spin-to-win/')
-                ->clickLink('login to play')
-                ->type('user_login', env('DORSET_USERNAME'))
-                ->click('#tokens-login-email-button')
-                ->type('user_password', env('DORSET_PASS'))
-                ->click('#tokens-login-password-button');
-        });
+        for ($i = 0; $i < 3; $i++) {
+
+            $this->browse(function (Browser $browser) {
+                $browser->visit('https://www.dorsetcereals.co.uk/collect-and-win/spin-to-win/')
+                    ->clickLink('login to play')
+                    ->type('user_login', env('DORSET_USERNAME'))
+                    ->click('#tokens-login-email-button')
+                    ->pause(1000)
+                    ->type('user_password', env('DORSET_PASS'))
+                    ->click('#tokens-login-password-button')
+                    ->waitForText('hi Lakshmi')
+                    ->click('img[src="https://dn3p03babliv5.cloudfront.net/app/uploads/wideimage/90ef5c82e0babf2f261654c5bcd1b2fc.png"]')
+                    ->pause(100000);
+            });
+        }
+
     }
 }
