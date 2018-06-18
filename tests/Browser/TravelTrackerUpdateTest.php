0e4bd8029636f25d0580dcee545e4dac9dd34331
Lakshmi Balakrishnan <lbalakrishnan@LBalakrishnan.local>
2018-06-18 11:46:57 +0100

    missing journeys

--- tests/Browser/TravelTrackerUpdateTest.php
+++ tests/Browser/TravelTrackerUpdateTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Browser;
 
-use Tests\DuskTestCase;
+use Facebook\WebDriver\WebDriverBy;
 use Laravel\Dusk\Browser;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\DuskTestCase;
 
 class TravelTrackerUpdateTest extends DuskTestCase
 {
@@ -20,9 +20,28 @@ class TravelTrackerUpdateTest extends DuskTestCase
                 ->type("email", "lbalakrishnan@brightsource.co.uk")
                 ->type("password", env("TRAVEL_TRACKER_PASSWORD"))
                 ->press('Login')
-                ->waitForText('My Journeys')
+                ->waitForText('Cycle')
                 ->select('#selectedJourney', "2")
-                ->press("Insert into every journey this week");
+                ->press("Insert into every journey this week")
+                ->waitForReload()
+                ->waitForText('Cycle');
+
+            if ((int) $browser->text('.badge') > 0) {
+
+                $response = $browser->visit("https://travel-tracker.net/app/132/user/missingjourneys")
+                    ->waitForText('Save');
+
+                for ($i = 1; $i <= $response->elements('select'); $i++) {
+                    $dropdown = $browser->driver->findElement(WebDriverBy::xpath("(//select)[$i]"));
+                    $action = $browser->driver->action();
+                    $action->click($dropdown)
+                        ->sendKeys($dropdown, "W")
+                        ->perform();
+                }
+
+                $browser->press("Save")
+                    ->waitForReload();
+            }
         });
     }
 }
